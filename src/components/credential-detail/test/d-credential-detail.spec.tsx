679d701034dc15cb6dd6a5f41aec42d627fd0cfe
phoebus-84 <ennio@dyne.org>
2024-01-31 11:28:22 +0100

    test: fix credential detail spec test

--- src/components/credential-detail/test/d-credential-detail.spec.tsx
+++ src/components/credential-detail/test/d-credential-detail.spec.tsx
@@ -10,7 +10,21 @@ describe('d-credential-detail', () => {
     expect(page.root).toEqualHtml(`
       <d-credential-detail>
         <mock:shadow-root>
-          <slot></slot>
+         <slot></slot>
+         <div>
+           <div class="heading">
+             <d-avatar></d-avatar>
+             <span class="name"></span>
+           </div>
+           <span class="description"></span>
+           <span class="long-description"></span>
+           <span class="issuer"></span>
+           <div class="w-full">
+             <d-button color="accent">
+               generate qr
+             </d-button>
+           </div>
+         </div>
         </mock:shadow-root>
       </d-credential-detail>
     `);
