da9e60503b22e33c1c91b54acfddae5401d56f77
phoebus-84 <ennio@dyne.org>
2024-06-26 18:59:47 +0200

    tests: fix scan button tests

--- src/components/scan-button/scan-button.stories.ts
+++ src/components/scan-button/scan-button.stories.ts
@@ -3,10 +3,14 @@ import type { Components } from '../../components.js';
 
 const meta = {
   title: 'Design System/Atoms/ScanButton',
-  render: ()=> `<d-scan-button>Scan</d-scan-button>`,
+  render: ()=> `<d-scan-button href="#">Scan</d-scan-button>`,
 } satisfies Meta<Components.DScanButton>;
 
 export default meta;
 type Story = StoryObj<Components.DScanButton>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args:{
+    href: '#'
+  }
+};
