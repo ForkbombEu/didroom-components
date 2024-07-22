c1cf8c7857ec381ee37b9848321b0f853b65f4f2
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2024-07-22 18:03:55 +0200

    fix: credential-service restyle (#121)
    * fix: credential-service restyle
    
    * fix: fix credential-service material design compliant

--- src/components/credential-service/test/d-credential-service.spec.tsx
+++ src/components/credential-service/test/d-credential-service.spec.tsx
@@ -10,15 +10,18 @@ describe('d-credential-service', () => {
     expect(page.root).toEqualHtml(`
       <d-credential-service>
         <mock:shadow-root>
-          <div>
-            <d-avatar size="l"></d-avatar>
-            <div class="flex flex-col grow">
-              <d-text size="l"></d-text>
-              <d-text size="s"></d-text>
-              <d-text size="xs"></d-text>
+          <div class="bg-primary flex gap-3 items-center no-underline p-5 rounded-lg w-full">
+            <div class="flex flex-grow gap-3 items-start">
+              <d-avatar shape="square" size="l"></d-avatar>
+              <div class="flex flex-col grow h-full justify-between min-h-[60px]">
+                <d-text size="l"></d-text>
+                <d-text class="!text-on-alt">
+                  Issuer_L:
+                </d-text>
+              </div>
             </div>
           </div>
-        </mock:shadow-root>
+       </mock:shadow-root>
       </d-credential-service>
     `);
   });
