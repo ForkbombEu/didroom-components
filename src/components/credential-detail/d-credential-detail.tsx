7ad589d19ad8238007eff3199a42b40845f520af
phoebus-84 <ennio@dyne.org>
2024-07-24 13:44:34 +0200

    fix: claims in credential-detail

--- src/components/credential-detail/d-credential-detail.tsx
+++ src/components/credential-detail/d-credential-detail.tsx
@@ -15,16 +15,16 @@ export class DCredentialDetail {
   render() {
     const longDescription = this.longDescription ? this.longDescription : '';
     return (
-      <Host>
-        <div class="frame">
-          <div class="heading">
-            <span class="name">{this.name}</span>
+      <Host class="w-full inline-flex flex-col items-start gap-2.5 px-0 py-5 rounded-lg bg-primary">
+        <div class="w-full flex flex-col items-start px-5 py-0 bg-primary">
+          <div class="flex text-on text-xl not-italic font-semibold items-center gap-2">
+            <span class="text-on text-2xl not-italic font-semibold leading-[20.5px] tracking-[-0.5px]">{this.name}</span>
           </div>
           <div class="info">
-            <span class="short-description">{this.description}</span>
-            <div class="details">
-              <span class="long-description">{longDescription}</span>
-              <span class="issuer">{this.issuer}</span>
+            <span class="text-on text-base not-italic font-medium leading-[20.5px] tracking-[-0.5px]">{this.description}</span>
+            <div class="flex flex-col items-start gap-4">
+              <span class="text-on-alt text-xs not-italic font-normal leading-[150%] tracking-[-0.5px]">{longDescription}</span>
+              <span class="self-stretch overflow-hidden text-on text-ellipsis text-xs not-italic font-normal leading-4">{this.issuer}</span>
             </div>
           </div>
           <div class="w-full">
