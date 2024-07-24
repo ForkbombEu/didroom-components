f7c7ed3d6474531b84629d919277b51581b63421
phoebus-84 <ennio@dyne.org>
2024-07-24 13:53:45 +0200

    update tests

--- src/components/credential-detail/test/d-credential-detail.spec.tsx
+++ src/components/credential-detail/test/d-credential-detail.spec.tsx
@@ -8,17 +8,17 @@ describe('d-credential-detail', () => {
       html: `<d-credential-detail></d-credential-detail>`,
     });
     expect(page.root).toEqualHtml(`
-      <d-credential-detail>
-        <mock:shadow-root>
-         <div class="frame">
-            <div class="heading">
-              <span class="name"></span>
-            </div>
+      <d-credential-detail class="bg-primary flex-col gap-2.5 inline-flex items-start px-0 py-5 rounded-lg w-full">
+       <mock:shadow-root>
+         <div class="bg-primary flex flex-col items-start px-5 py-0 w-full">
+           <div class="flex font-semibold gap-2 items-center not-italic text-on text-xl">
+             <span class="font-semibold leading-[20.5px] not-italic text-2xl text-on tracking-[-0.5px]"></span>
+           </div>
            <div class="info">
-             <span class="short-description"></span>
-             <div class="details">
-               <span class="long-description"></span>
-               <span class="issuer"></span>
+             <span class="font-medium leading-[20.5px] not-italic text-base text-on tracking-[-0.5px]"></span>
+             <div class="flex flex-col gap-4 items-start">
+               <span class="font-normal leading-[150%] not-italic text-on-alt text-xs tracking-[-0.5px]"></span>
+               <span class="font-normal leading-4 not-italic overflow-hidden self-stretch text-ellipsis text-on text-xs"></span>
              </div>
            </div>
            <div class="w-full">
