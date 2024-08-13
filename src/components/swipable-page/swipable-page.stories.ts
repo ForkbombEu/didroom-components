dabac5c8a8168506e0168598dbd7b5c2ebf359e2
phoebus-84 <ennio@dyne.org>
2024-08-13 13:01:35 +0200

    docs: add swipable page story

---
+++ src/components/swipable-page/swipable-page.stories.ts
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/html';
+import type { Components } from '../../components.js';
+
+const meta = {
+  title: 'Design System/Molecule/SwipablePage',
+  render: args => `<d-swipable-page 
+    background="${args.background}"
+    title="${args.title}"
+    subtitle="${args.subtitle}"
+    description="${args.description}"
+  >
+    <d-illustration illustration="logo-hand" />
+    </d-swipable-page>`,
+} satisfies Meta<Components.DSwipablePage>;
+
+export default meta;
+type Story = StoryObj<Components.DSwipablePage>;
+
+export const Default: Story = {
+  args: {
+    background: 'https://wallet.zenswarm.forkbomb.eu/src/lib/assets/bg-1.svg',
+    title: 'Title',
+    subtitle: 'Subtitle',
+    description: 'Description',
+  },
+};
