72bdb8e0d3d39642a18834bf458cdd2087ca8c66
Puria Nafisi Azizi <puria@dyne.org>
2024-10-11 12:09:49 +0200

    Update credential-card.stories.ts

--- src/components/credential-card/credential-card.stories.ts
+++ src/components/credential-card/credential-card.stories.ts
@@ -29,7 +29,7 @@ export const Default: Story = {
     expirationLabel: 'exp',
     expirationDate: '2017-01-01',
     verified: false,
-    logoSrc: `https://api.dicebear.com/9.x/icons/svg?seed=${new Date()}`,
+    logoSrc: `https://api.dicebear.com/9.x/shapes/svg?seed=${new Date()}`,
   },
   parameters: {
     design: {
