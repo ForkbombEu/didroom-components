aacf8f459d23896c53caf14dab2e3cb25fe0cf28
phoebus-84 <ennio@dyne.org>
2024-10-17 11:19:18 +0200

    fix: scanner mask missing background

--- src/components/d-scanner-mask/d-scanner-mask.tsx
+++ src/components/d-scanner-mask/d-scanner-mask.tsx
@@ -55,17 +55,16 @@ export class DScannerMask {
     return (
       <Host>
         <div class="visible fixed left-0 top-0 z-40 flex h-screen w-full flex-col items-center justify-center">
-          <div class="bg-surface bg-opacity-70 min-h-24 w-full flex-grow" />
+          <div class="dark:bg-[#253151] bg-[#e9ebef] bg-opacity-70 min-h-24 w-full flex-grow" />
           <div class="flex h-72 w-full">
-            <div class="max-w-1/4 bg-surface bg-opacity-70 h-full flex-grow" />
+            <div class="max-w-1/4 dark:bg-[#253151] bg-[#e9ebef] bg-opacity-70 h-full flex-grow" />
             <div class="viewfinder relative z-50 h-72 w-72 overflow-hidden rounded-md bg-transparent">
               <div class="absolute left-0 top-0 h-full w-full border-8 border-accent"></div>
               <div class="absolute left-0 top-1/2 h-1 w-full -translate-y-1/2 transform bg-accent" style={style}></div>
             </div>
-            <div class="bg-surface bg-opacity-70 h-full flex-grow" />
+            <div class="dark:bg-[#253151] bg-[#e9ebef] bg-opacity-70 h-full flex-grow" />
           </div>
-
-          <div class="bg-surface bg-opacity-70 w-full flex-grow">
+          <div class="dark:bg-[#253151] bg-[#e9ebef] bg-opacity-70 w-full flex-grow">
             <div class="text-center justify-center">
               <d-page-description title={this.heading} description={this.description} />
             </div>
