9b04903bb6cda51a4b10403e3d41bd5010026dff
matteo-cristino <cristino.matteo97@gmail.com>
2025-11-24 13:43:45 +0100

    test: update definition test

--- src/components/definition/test/d-definition.spec.tsx
+++ src/components/definition/test/d-definition.spec.tsx
@@ -11,7 +11,7 @@ describe('d-definition', () => {
       <d-definition class="border-b border-b-on-alt border-solid flex justify-between w-full">
        <mock:shadow-root>
          <div class="border-b border-b-on-alt border-solid flex justify-between w-full">
-           <dl class="flex flex-col h-11 w-full">
+           <dl class="flex flex-col min-h-11 w-full">
              <dt class="font-normal leading-[150%] not-italic text-on-alt text-xs tracking-[-0.5px]"></dt>
              <dd class="font-medium leading-[150%] not-italic text-on text-xs tracking-[-0.5px]"></dd>
         </mock:shadow-root>
